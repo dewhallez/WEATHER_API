3ba62bb071c8525568a5a5d43be7226d49d665bd
akinwale <4412196+dewhallez@users.noreply.github.com>
2021-03-31 13:44:35 -0400

    add html and css codes

--- app.py
+++ app.py
@@ -2,14 +2,50 @@ from flask import Flask, render_template, request
 import requests
 import configparser
 
+# create function to get API Key from config.ini file
+def get_api_key():
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    return config['openweathermap']['api']
 
 
 app = Flask(__name__)
 
-
+# Route to home page
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def weather_dashboard():
+    return render_template('home.html')
+
+# Route to results
+@app.route('/results', methods=['POST'])
+def render_results():
+    zip_code = request.form['zipCode']
+
+    api_key = get_api_key()
+    data = get_weather_results(zip_code, api_key)
+    temp = "{0:.2f}".format(data['main']["temp"])
+    feels_like = "{0:,2f}".format(data["main"]["feels_like"])
+    weather = data["weather"][0]["main"]
+    location = data["name"]
+
+    return render_template('results.html', location=location, temp=temp, feels_like=feels_like, weather=weather)
+
+
+# Get api key from "config.init" file
+def get_api_key():
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    return config['openweathermap']['api']
+
+
+# Get weather for a specified zip code
+def get_weather_results(zip_code, api_key):
+    api_url = "https://api.openweathermap.org/data/2.5/weather?zip={}&units=imperial&appid={}".format(zip_code, api_key)
+    r = requests.get(api_url)
+    return r.json()
+
+# print out weather for a desired zip code
+#print(get_weather_results('30122', get_api_key()))
 
 
 if __name__ == '__main__':
